f0f1d9c1892435b3c756fed8e512033442c3a149
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-28 15:08:26 -0800

    fix a deathly slow regex

--- src/helpers.py
+++ src/helpers.py
@@ -55,7 +55,7 @@ def clean(text, author=None):
     for prefix in bot_prefixes:
         if text.lower().startswith(prefix): return None #handle bot commands
     
-    text= re.sub(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&=]*)|\S+@\S+|(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}|```(.*\n)+```|:(.*)+:', "", text) #remove urls, emails, code blocks, custom emojis, and phone numbers
+    text= re.sub(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)|\S+@\S+|(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}|```(.*\n)+```|:[^:\s]*(?:::[^:\s]*)*:', "", text) #remove urls, emails, code blocks, custom emojis, and phone numbers
     temp=""
     for char in text.strip():
         convi=None
@@ -72,7 +72,7 @@ def clean(text, author=None):
     text= re.sub(r'[\U00003000\U0000205F\U0000202F\U0000200A\U00002000-\U00002009\U00001680\U000000A0\U00000020]', " ", text) #handle... interesting spaces
     text= "".join([normalize_chars[char] if char in normalize_chars else char for char in text.strip()]) #handle special chars from other langs
     text= re.sub(r'([:.,!?@\'\"]|\\n) ([:.,!?\'\"]|\\n)', r'\1\2', text) #handle extraneous spaces between punctuation    
-    text= re.sub(r"[^A-Za-z1-9.!@?\"\'\s\U0001F600-\U0001F64F\U0001F300-\U0001F5FF]+", "",text.strip()) #handle non-emoji, punctuation, and letters
+    text= re.sub(r"[^A-Za-z1-9.,!@?\"\'\s\U0001F600-\U0001F64F\U0001F300-\U0001F5FF]+", "",text.strip()) #handle non-emoji, punctuation, and letters
     text= re.sub(r"(?i)([\.\'\"@?!a-z])\1{3,}", r"\1\1\1", text.strip()) #handle excessive repeats of punctuation, limited to 3
     text= re.sub(r"(?i)\s(.+?)\1+\s", r" \1 ", text.strip()) #handle repeated words
     if author == None: text= re.sub(r'@Deleted User', gen_name, text) #replace "deleted users" with names

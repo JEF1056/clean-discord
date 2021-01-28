02ac63d870038086a5cf0a028bcfde4cb72b2147
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-28 00:43:53 -0800

    remove the whole multithreading thing

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,11 @@
 import io
 import re
 import random
+import threading
 import argparse
+import time
+from datetime import datetime
+from math import log
 
 alphabets=io.open("src/alphabets.txt", mode="r", encoding="utf-8").read().strip().split("\n")
 names=io.open("src/names.txt", mode="r", encoding="utf-8").read().strip().split("\n")
@@ -15,6 +19,46 @@ normalize_chars={'Š':'S', 'š':'s', 'Ð':'Dj','Ž':'Z', 'ž':'z', 'À':'A', 'Á
     'ú':'u', 'û':'u', 'ü':'u', 'ý':'y', 'ý':'y', 'þ':'b', 'ÿ':'y', 'ƒ':'f',
     'ă':'a', 'î':'i', 'â':'a', 'ș':'s', 'ț':'t', 'Ă':'A', 'Î':'I', 'Â':'A', 'Ș':'S', 'Ț':'T',}
 
+class worker(threading.Thread):
+    def __init__(self, filename, ilist, olist, pbar, disposed, completed, args):
+        threading.Thread.__init__(self)
+        self.filename, self.ilist, self.olist = filename, ilist, olist
+        self.pbar, self.disposed, self.completed, self.args = pbar, disposed, completed, args
+
+    def run(self):
+        self.pbar.set_description(f"{threading.active_count()} active workers")
+        last_known_name=""
+        last_known_time=0
+        build=""
+        temp=[]
+        title=self.filename.split(" - ")
+        try: part=re.findall(r"\[part (\d)\]",self.filename)[0]
+        except: part=0
+        for curr_message in self.ilist:
+            msg=clean(curr_message["content"])
+            if msg != None:
+                if curr_message["author"]["name"] != last_known_name:
+                    last_known_name=curr_message["author"]["name"]
+                    build+=f"\t{clean(last_known_name,author=curr_message['author']['id'])}: {msg}"
+                else:
+                    build+="\\n"+msg
+            else:self.disposed+=1
+            today=time.mktime(datetime.strptime(curr_message["timestamp"].split(".")[0].replace("+00:00",""), "%Y-%m-%dT%H:%M:%S").timetuple())
+            if today-last_known_time > self.args.conversation_timeout and last_known_time != 0:
+                if build.startswith("\t"): build=build[1:]
+                if build.startswith("\\n"): build=build[2:]
+                if build.count("\t") > 1 and build != "":
+                    temp.append(build.replace("\n","")+"\n")
+                    self.completed+=1
+                build=""
+                last_known_name=""
+            last_known_time=today
+                
+            if self.args.workers == 1: self.pbar.set_description(f'{title[0]} - {title[1]} - Part {part}, Conversations: {self.completed} Removed: {self.disposed}')
+            self.pbar.update(1)
+        self.olist.append(temp)
+        self.pbar.set_description(f"{threading.active_count()} active workers")
+
 def str2bool(v):
     if isinstance(v, bool):
        return v
@@ -25,12 +69,19 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
+unit_list = zip(['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'], [0, 0, 1, 2, 2, 2])
+def sizeof_fmt(num):
+    """Human friendly file size"""
+    if num > 1:
+        exponent = min(int(log(num, 1024)), len(unit_list) - 1)
+        quotient = float(num) / 1024**exponent
+        unit, num_decimals = unit_list[exponent]
+        format_string = '{:.%sf} {}' % (num_decimals)
+        return format_string.format(quotient, unit)
+    if num == 0:
+        return '0 bytes'
+    if num == 1:
+        return '1 byte'
 
 def gen_name(username):
     try:
@@ -65,15 +116,13 @@ def clean(text, author=None):
     text= re.sub(r'([:.,!?@\'\"]|\\n) ([:.,!?\'\"]|\\n)', r'\1\2', text) #handle extraneous spaces between punctuation    
     text= re.sub(r"[^A-Za-z1-9.!@?\"\'\s\U0001F600-\U0001F64F\U0001F300-\U0001F5FF]+", "",text.strip()) #handle non-emoji, punctuation, and letters
     text= re.sub(r"(?i)([\.a-z])\1{3,}", r"\1\1\1", text.strip()) #handle excessive repeats of letters or ...
-        #text= re.sub(r"([A-Za-z])\.{2,}", r"\1 ... ", text.strip())
     text= re.sub(r"([\s!?@\"\'])\1+", r"\1",text.strip()) #handle excessive spaces or excessive punctuation
-    text= re.sub(r'\s([?.!\"](?:\s|$))', r'\1', text) #handle spaces before punctuation but after text
+    #text= re.sub(r'\s([?.!\"](?:\s|$))', r'\1', text) #handle spaces before punctuation but after text
     text= text.replace("\n","\\n") #handle newlines
     
     if text != "\\n" and text != " " and text != "" and author==None:
         return text
     elif text != "\\n" and text != " " and text != "" and text != "Deleted User" and author!=None:
-        # add code to replace names
         return text
     elif author!=None:
         return gen_name(author)

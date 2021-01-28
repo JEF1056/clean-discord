831d0ad34f968af3892c722c49aaaa4ec0ce6a2f
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-28 10:32:54 -0800

    refactor

--- convert.py
+++ convert.py
@@ -21,7 +21,7 @@ parser.add_argument("-disable_description", type=str2bool, nargs='?', const=True
                     help="disable TQDM description")
 parser.add_argument("-cache", type=str2bool, nargs='?', const=True, default=False,
                     help="turn on cache when reading files (uses a lot of memory)")
-parser.add_argument('-step', type=int, default=0, choices=[0,1],
+parser.add_argument('-step', type=str, default="clean", choices=["clean", "nontoxic"],
                     help='TQDM update interval')
 
 parser.add_argument("-nontoxic", type=str2bool, nargs='?', const=True, default=False,
@@ -40,7 +40,7 @@ disposed=0
 completed=0
 len_all_messages=1
 
-if args.step < 1:
+if args.step == "clean":
     all_messages={} if args.cache else 0
     with tqdm(os.listdir(args.dir), desc="Reading files") as pbar:
         for file in pbar:
@@ -64,62 +64,63 @@ if args.step < 1:
                 build=""
                 last_id=re.findall(r"\[\d{18,}\]",file)[0]
             for curr_message in all_messages[file]:
-                msg=clean(curr_message["content"])
-                if msg != None:
-                    if curr_message["author"]["name"] != last_known_name:
-                        last_known_name=curr_message["author"]["name"]
-                        build+=f"\t{clean(last_known_name,author=curr_message['author']['id'])}: {msg}"
-                    else:
-                        build+="\\n"+msg
-                else:disposed+=1
-                try: today=time.mktime(datetime.strptime(curr_message["timestamp"].split(".")[0].replace("+00:00",""), "%Y-%m-%dT%H:%M:%S").timetuple())
-                except: print(curr_message["timestamp"])
-                if today-last_known_time > args.conversation_timeout and last_known_time != 0:
-                    if build.startswith("\t"): build=build[1:]
-                    if build.startswith("\\n"): build=build[2:]
-                    if build.count("\t") > 1 and build != "":
-                        f.write(build.replace("\n","")+"\n")
-                        completed+=1
-                    build=""
-                    last_known_name=""
-                last_known_time=today
-                    
-                title=file.split(" - ")
-                try:
-                    part=re.findall(r"\[part (\d)\]",file)[0]
-                except:
-                    part=0
-                if not args.disable_description: pbar.set_description(f'{title[0]} - {title[1]} - Part {part}, Conversations: {completed} Removed: {disposed}')
-                pbar.update(1)
+                if not message["author"]["isBot"]:
+                    msg=clean(curr_message["content"])
+                    if msg != None:
+                        if curr_message["author"]["name"] != last_known_name:
+                            last_known_name=curr_message["author"]["name"]
+                            build+=f"\t{clean(last_known_name,author=curr_message['author']['id'])}: {msg}"
+                        else:
+                            build+="\\n"+msg
+                    else:disposed+=1
+                    try: today=time.mktime(datetime.strptime(curr_message["timestamp"].split(".")[0].replace("+00:00",""), "%Y-%m-%dT%H:%M:%S").timetuple())
+                    except: print(curr_message["timestamp"])
+                    if today-last_known_time > args.conversation_timeout and last_known_time != 0:
+                        if build.startswith("\t"): build=build[1:]
+                        if build.startswith("\\n"): build=build[2:]
+                        if build.count("\t") > 1 and build != "":
+                            f.write(build.replace("\n","")+"\n")
+                            completed+=1
+                        build=""
+                        last_known_name=""
+                    last_known_time=today
+                        
+                    title=file.split(" - ")
+                    try:
+                        part=re.findall(r"\[part (\d)\]",file)[0]
+                    except:
+                        part=0
+                    if not args.disable_description: pbar.set_description(f'{title[0]} - {title[1]} - Part {part}, Conversations: {completed} Removed: {disposed}')
+                    pbar.update(1)
+                else: disposed+=1
     del all_messages
 
-if args.step < 2:
-    if args.nontoxic:
-        from tox_block.prediction import make_predictions as detect       
-        to_clean=io.open(os.path.join(args.out,"context.txt"), mode="r", encoding="utf-8").read().strip().split("\n")
-        with io.open(os.path.join(args.out,"context-detox.txt"), mode="w", encoding="utf-8") as f:
-            with tqdm(to_clean, desc="Processing messages") as pbar:
-                batch=[]
-                for curr_index,conversation in enumerate(pbar):
-                    batch.append(conversation)
-                    if curr_index==len(to_clean)-1 or sum([len(msgs.strip().split("\t")) for msgs in batch]) >= args.batches:
-                        batch_placement,sents=[0],[]
-                        for conv in batch:
-                            splt=[e for e in conv.strip().split("\t") if e != ""]
-                            sents.extend(splt)
-                            batch_placement.append(len(splt))
-                        prediction_vals=detect(sents)
-                        scores=[max(list(dict(prediction_vals[detection]).values())[1:]) for detection in prediction_vals]
-                        offsets=[sum(batch_placement[0:i]) for i in range(1,len(batch_placement))]
-                        for ind, batch_score in enumerate([scores[sum(batch_placement[0:i]):sum(batch_placement[0:i])+batch_placement[i]] for i in range(1,len(batch_placement))]):
-                            to_write=[]
-                            for i,v in enumerate(batch_score):
-                                if v <= args.confidence: to_write.append(sents[offsets[ind]+i])
-                                else: disposed_tox+=1
-                            to_write="\t".join(to_write)
-                            f.write(to_write+"\n")
-                        pbar.set_description(f"Batch of {len(sents)}, Removed {disposed_tox}")
-                        batch=[]
+if args.step == "nontoxic" or args.nontoxic:
+    from tox_block.prediction import make_predictions as detect       
+    to_clean=io.open(os.path.join(args.out,"context.txt"), mode="r", encoding="utf-8").read().strip().split("\n")
+    with io.open(os.path.join(args.out,"context-detox.txt"), mode="w", encoding="utf-8") as f:
+        with tqdm(to_clean, desc="Processing messages") as pbar:
+            batch=[]
+            for curr_index,conversation in enumerate(pbar):
+                batch.append(conversation)
+                if curr_index==len(to_clean)-1 or sum([len(msgs.strip().split("\t")) for msgs in batch]) >= args.batches:
+                    batch_placement,sents=[0],[]
+                    for conv in batch:
+                        splt=[e for e in conv.strip().split("\t") if e != ""]
+                        sents.extend(splt)
+                        batch_placement.append(len(splt))
+                    prediction_vals=detect(sents)
+                    scores=[max(list(dict(prediction_vals[detection]).values())[1:]) for detection in prediction_vals]
+                    offsets=[sum(batch_placement[0:i]) for i in range(1,len(batch_placement))]
+                    for ind, batch_score in enumerate([scores[sum(batch_placement[0:i]):sum(batch_placement[0:i])+batch_placement[i]] for i in range(1,len(batch_placement))]):
+                        to_write=[]
+                        for i,v in enumerate(batch_score):
+                            if v <= args.confidence: to_write.append(sents[offsets[ind]+i])
+                            else: disposed_tox+=1
+                        to_write="\t".join(to_write)
+                        f.write(to_write+"\n")
+                    pbar.set_description(f"Batch of {len(sents)}, Removed {disposed_tox}")
+                    batch=[]
 
 print(f"Removed {disposed}+{disposed_tox}/{len_all_messages}, {round((disposed+disposed_tox)/len_all_messages,2)}%")
 print(f"Dataset final size: {len_all_messages - disposed - disposed_tox} messages, reduced from {sizeof_fmt(sum([os.path.getsize(f'{os.path.join(args.dir,fle)}') >> 20 for fle in os.listdir(args.dir)]))} to {sizeof_fmt(os.path.getsize(os.path.join(args.out,'context-detox.txt'))) if args.nontoxic else sizeof_fmt(os.path.getsize(os.path.join(args.out,'context.txt')))}")

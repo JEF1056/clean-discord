7f01453890acf2d811747f8fbf2c42cc40a4d0b7
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-27 23:26:48 -0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -30,6 +30,7 @@ args = parser.parse_args()
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+assert args.workers <= len(os.listdir(args.dir))
 
 all_messages=0 if args.memory_efficient else {} 
 with tqdm(os.listdir(args.dir), desc="Reading files") as pbar:

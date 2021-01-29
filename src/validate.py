4d1d203b1956b6cc020cc6278ed878b33962abbf
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-28 18:07:39 -0800

    Update validate.py

--- src/validate.py
+++ src/validate.py
@@ -1,15 +1,18 @@
 import os
 
-def check_files():
+def check_files(server="None"):
     failed=[]
     for file in os.listdir("data"):
-        with open(os.path.join("data",file), 'rb') as f:
-            f.seek(-2, os.SEEK_END)
-            while f.read(1) != b' ':
-                f.seek(-2, os.SEEK_CUR)
-            last_line = f.readline().decode()
-            try: int(last_line)
-            except: failed.append(f"{file} ||| {last_line}")
+        if file.startswith(server) or server=="":
+            with open(os.path.join("data",file), 'rb') as f:
+                f.seek(-2, os.SEEK_END)
+                while f.read(1) != b' ':
+                    f.seek(-2, os.SEEK_CUR)
+                last_line = f.readline().decode()
+                try: int(last_line)
+                except: failed.append(f"{file} ||| {last_line}")
 
     assert failed == [], f"Some files in data directory are incomplete downloads:"+"\n".join(failed)
     return "All files passed."
+
+print(check_files(server="a crumb"))

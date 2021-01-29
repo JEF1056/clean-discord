1a19b747d33765c52c4f86f27ef4d452f584855b
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-28 21:48:45 -0800

    Update validate.py

--- src/validate.py
+++ src/validate.py
@@ -1,8 +1,11 @@
 import os
+import io
+import json
+from tqdm import tqdm
 
-def check_files(server="None"):
+def check_files(dir, server="None"):
     failed, num_processed=[],0
-    for file in os.listdir("data"):
+    for file in os.listdir(dir):
         if file.startswith(server) or server=="":
             with open(os.path.join("data",file), 'rb') as f:
                 f.seek(-2, os.SEEK_END)
@@ -13,8 +16,19 @@ def check_files(server="None"):
                 except: failed.append(f"{file} ||| {last_line}")
                 num_processed+=1
 
-    assert num_processed != 0, "No files processed"
-    assert failed == [], f"Some files in data directory are incomplete downloads:"+"\n".join(failed)
+    assert num_processed != 0, f"No files processed/No files in dir:{dir}"
+    if failed != []:
+        if input("Found errors:\n\n"+"\n".join(failed)+"\n\nFix files? Type one of the following:  [y,yes]\n> ") in ["yes",'y']:
+            fix_files(dir, [i.split(" ||| ")[0] for i in failed])
+        else: assert failed == [], f"Some files in data directory are incomplete downloads (see above)"
     return "All files passed."
 
-#print(check_files(server="a crumb"))
+def fix_files(dir, filenames):
+    assert type(filenames)==list
+    for filename in tqdm(filenames, desc="Fixing files"):
+        file_data=io.open(os.path.join(dir,filename), mode="r", encoding="utf-8").read()+"]}"
+        file_data = json.loads(file_data)
+        len_file_data=str(len(file_data["messages"]))
+        del file_data
+        with io.open(os.path.join(dir,filename), mode="a", encoding="utf-8") as f:
+            f.write('\n  ],\n  "messageCount": '+len_file_data+'\n}')

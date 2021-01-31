6edc17c2e10050bd081ebcee671789606597dcd3
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-30 20:41:38 -0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -199,7 +199,7 @@ if args.step == "clean":
         for file in all_data_files:  # loop through each file containing messages
             
             if len(threads) == args.threads:
-                print("Thread cap reached, waiting...")
+                pbar.set_description(f"Thread cap reached, waiting ...")
                 x = threads.pop(0)
                 x.join()
 

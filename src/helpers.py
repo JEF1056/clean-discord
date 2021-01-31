f62da9afc93b6a8228535442517b76d185653343
Jess Edmund Fan <jessefan2002@gmail.com>
2021-01-30 21:42:51 -0800

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -44,21 +44,21 @@ def gen_name(username):
     except: return "@"+random.choice(names)
     
 #precompile regex
-r1=re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)|\S+@\S+|(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}|```(.*\n)+```|:[^:\s]*(?:::[^:\s]*)*:|\\n', concurrent=True)
-r2=re.compile(r'[\U00003000\U0000205F\U0000202F\U0000200A\U00002000-\U00002009\U00001680\U000000A0\U00000020]',concurrent=True)
-r3=re.compile(r'([:.,!?@\'\"]|\\n) ([:.,!?\'\"]|\\n)',concurrent=True)
-r4=re.compile(r"[^a-z1-9.,!@?\"\'\s\U0001F600-\U0001F64F\U0001F300-\U0001F5FF]+",re.IGNORECASE,concurrent=True)
-r5=re.compile(r"([\.\'\"@?!a-z])\1{3,}", re.IGNORECASE,concurrent=True)
-r6=re.compile(r"\s(.+?)\1+\s", re.IGNORECASE,concurrent=True)
-r7=re.compile(r'@Deleted User',concurrent=True)
-r8=re.compile(r"([\s!?@\"\'])\1+",concurrent=True)
-r9=re.compile(r'\s([?.!\"](?:\s|$))',concurrent=True)
+r1=re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)|\S+@\S+|(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}|```(.*\n)+```|:[^:\s]*(?:::[^:\s]*)*:|\\n')
+r2=re.compile(r'[\U00003000\U0000205F\U0000202F\U0000200A\U00002000-\U00002009\U00001680\U000000A0\U00000020]')
+r3=re.compile(r'([:.,!?@\'\"]|\\n) ([:.,!?\'\"]|\\n)')
+r4=re.compile(r"[^a-z1-9.,!@?\"\'\s\U0001F600-\U0001F64F\U0001F300-\U0001F5FF]+",re.IGNORECASE)
+r5=re.compile(r"([\.\'\"@?!a-z])\1{3,}", re.IGNORECASE)
+r6=re.compile(r"\s(.+?)\1+\s", re.IGNORECASE)
+r7=re.compile(r'@Deleted User')
+r8=re.compile(r"([\s!?@\"\'])\1+")
+r9=re.compile(r'\s([?.!\"](?:\s|$))')
 
 def clean(text, author=None):
     for prefix in bot_prefixes:
         if text.lower().startswith(prefix): return None #handle bot commands
         
-    text= re.sub(r1, "", text) #remove urls, emails, code blocks, custom emojis, and phone numbers
+    text= re.sub(r1, "", text,concurrent=True) #remove urls, emails, code blocks, custom emojis, and phone numbers
     temp=""
     for char in text.strip():
         convi=None
@@ -72,15 +72,15 @@ def clean(text, author=None):
         if convi==None: temp+=char
     text= temp
     text= text.replace("\t"," ") #handle tabs
-    text= re.sub(r2, " ", text) #handle... interesting spaces
+    text= re.sub(r2, " ", text,concurrent=True) #handle... interesting spaces
     text= "".join([normalize_chars[char] if char in normalize_chars else char for char in text.strip()]) #handle special chars from other langs
-    text= re.sub(r3, r'\1\2', text) #handle extraneous spaces between punctuation    
-    text= re.sub(r4, "",text.strip()) #handle non-emoji, punctuation, and letters
-    text= re.sub(r5, r"\1\1\1", text.strip()) #handle excessive repeats of punctuation, limited to 3
-    text= re.sub(r6, r" \1 ", text.strip()) #handle repeated words
+    text= re.sub(r3, r'\1\2', text,concurrent=True) #handle extraneous spaces between punctuation    
+    text= re.sub(r4, "",text.strip(),concurrent=True) #handle non-emoji, punctuation, and letters
+    text= re.sub(r5, r"\1\1\1", text.strip(),concurrent=True) #handle excessive repeats of punctuation, limited to 3
+    text= re.sub(r6, r" \1 ", text.strip(),concurrent=True) #handle repeated words
     if author == None: text= re.sub(r7, gen_name, text) #replace "deleted users" with names
-    text= re.sub(r8, r"\1",text.strip()) #handle excessive spaces or excessive punctuation
-    text= re.sub(r9, r'\1', text) #handle spaces before punctuation but after text
+    text= re.sub(r8, r"\1",text.strip(),concurrent=True) #handle excessive spaces or excessive punctuation
+    text= re.sub(r9, r'\1', text,concurrent=True) #handle spaces before punctuation but after text
     text= text.replace("\n","\\n") #handle newlines
     
     if text != "\\n" and text != " " and text != "" and author==None:

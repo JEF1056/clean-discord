045f22cc9386dc0b129930578c5115a08bb0a216
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-22 11:36:43 -0700

    no mp

--- split.py
+++ split.py
@@ -10,61 +10,22 @@ parser.add_argument('-dir', type=str, default="data",
                     help='the data folder containing the processed files on the top level')
 parser.add_argument('-out', type=str, default="context",
                     help='prefix the compressed output file sets')
-parser.add_argument('-workers', type=int, default=None,
-                    help='the folder to output txts')
 parser.add_argument('-compression_level', type=int, default=9, choices=list(range(-1,10)),
                     help='how compressed the file should be')
 args = parser.parse_args()
 
-if args.workers == None: args.workers= mp.cpu_count()*2
-
-def convertline(text, max_length=10):
-    text=text.split("\t") #split the conversation by tabs
-    inputs, targets=[],[] #create empty arrays for inputs and targets
-    for y in range(1,len(text)): #iterate through the split conversation
-        x=y-max_length if y-max_length >= 0 else 0 #get the starting value; if it's negative, use 0 instead
-        inputs.append("/b".join(text[x:y])) #append to the inputs the current window, joined by /b
-        targets.append(text[y]) #append the target
-    return [f"{inputs[i]}\t{targets[i]}" for i in range(len(inputs))] #zip them together in a dict of inputs and targets
-
-def worker(filename, q):
-    with io.open(os.path.join(args.dir, filename), mode="r", encoding="utf-8") as f:
-        line = f.readline()
-        while line:
-            q.put(convertline(line.strip()))
-            line=f.readline()
+def worker(files, split, max_length=10):
+    for filename in tqdm(files, desc=f"{split} split..."):
+        with io.open(os.path.join(args.dir, filename), mode="r", encoding="utf-8") as f:
+            line = f.readline()
+            while line:
+                text=text.split()
+                with io.open(f"{args.out}-{split}.txt", "w") as f:
+                    for y in range(1,len(text)):
+                        x=y-max_length if y-max_length >= 0 else 0
+                        f.write(f"{'/b'.join(text[x:y])}\t{text[y]}")
+                line=f.readline()
     return "DONE"
-
-def listener(q, split):
-    fst=True
-    with open(f"{args.out}-{split}.zlib", mode='wb') as f:
-        while 1:
-            m = q.get()
-            for line in m:
-                if fst: f.write(zlib.compress(line, args.compression_level)); fst=False
-                else: f.write(zlib.compress("\n"+line, args.compression_level))
-                f.flush()
-
-def main(files, split, q):
-    #must use Manager queue here, or will not work
-    pool = mp.Pool(args.workers)
-
-    #put listener to work first
-    watcher = pool.apply_async(listener, (q, split))
-
-    #fire off workers
-    jobs = []
-    for file in files:
-        job = pool.apply_async(worker, (file, q))
-        jobs.append(job)
-
-    # collect results from the workers through the pool result queue
-    for job in tqdm(jobs, desc=f"Processing {split}..."): 
-        job.get()
-
-    #now we are done, kill the listener
-    pool.close()
-    pool.join()
 
 if __name__ == '__main__':
     files=os.listdir(args.dir)
@@ -72,9 +33,6 @@ if __name__ == '__main__':
     files.remove('stats.json')
     cut_off = int(len(files) * .05)
     train_files, eval_files = files[:-cut_off], files[-cut_off:]
-    manager = mp.Manager()   
     
-    q = manager.Queue() 
-    main(train_files, "train", q)
-    q = manager.Queue() 
-    main(eval_files, "val", q)
+    worker(train_files, "train")
+    worker(eval_files, "val")

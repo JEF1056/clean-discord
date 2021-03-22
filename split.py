97c5d2817b3ebfe6163666edf5d336ee2994bee7
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-22 11:54:36 -0700

    update

--- split.py
+++ split.py
@@ -13,16 +13,15 @@ parser.add_argument('-compression_level', type=int, default=9, choices=list(rang
                     help='how compressed the file should be')
 args = parser.parse_args()
 
-def worker(files, split, max_length=10):
+def worker(files, split, w, max_length=10):
     for filename in tqdm(files, desc=f"{split} split..."):
         with io.open(os.path.join(args.dir, filename), mode="r", encoding="utf-8") as f:
             line = f.readline()
             while line:
                 line=line.split()
-                with open(f"{args.out}-{split}.txt", "wb") as w:
-                    for y in range(1,len(line)):
-                        x=y-max_length if y-max_length >= 0 else 0
-                        w.write(zlib.compress(f"{'/b'.join(line[x:y])}\t{line[y]}".encode()))
+                for y in range(1,len(line)):
+                    x=y-max_length if y-max_length >= 0 else 0
+                    w.write(zlib.compress(f"{'/b'.join(line[x:y])}\t{line[y]}".encode()))
                 line=f.readline()
     return "DONE"
 
@@ -33,5 +32,6 @@ if __name__ == '__main__':
     cut_off = int(len(files) * .05)
     train_files, eval_files = files[:-cut_off], files[-cut_off:]
     
-    worker(train_files, "train")
-    worker(eval_files, "val")
+    with open(f"{args.out}-train.txt", "wb") as w:
+        worker(train_files, "train", w)
+    #worker(eval_files, "val")

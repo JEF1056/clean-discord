17be9710ecf3a432805a9d078757f3b60e9e96b7
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-22 10:22:31 -0700

    push a fix including a split creator

--- split.py
+++ split.py
@@ -1,76 +1,73 @@
-import io
 import os
-from tqdm import tqdm
+import io
 import argparse
-import numpy as np
-from src.helpers import str2bool as s2b
+from tqdm import tqdm
+import multiprocessing as mp
 
-parser = argparse.ArgumentParser(description='Clean Discord data')
+parser = argparse.ArgumentParser(description='Split dataset in multiple files into train and validation sets')
 parser.add_argument('-dir', type=str, default="data",
-                    help='the data folder containing preprocessed data to merge and split')
+                    help='the data folder containing the processed files on the top level')
 parser.add_argument('-out', type=str, default="context",
-                    help='prefix for the files to be written')
-parser.add_argument('-div_symbol', type=str, default="/b",
-                    help='prefix for the files to be written')
-parser.add_argument('-newline_symbol', type=str, default="/n",
-                    help='prefix for the files to be written')
-parser.add_argument('-split', type=float, default=0.95,
-                    help='split% for training data')
-parser.add_argument('-chunks', type=int, default=20,
-                    help='max length of the dataset')
-parser.add_argument('-max_len', type=int, default=2048,
-                    help='max length of the dataset')
-parser.add_argument("-ascii", type=s2b, nargs='?', const=True, default=False,
-                    help="debugging flag")
-parser.add_argument("-shuffle", type=s2b, nargs='?', const=True, default=False,
-                    help="debugging flag")
+                    help='prefix the compressed output file sets')
 args = parser.parse_args()
 
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield "\t".join(lst[i:i + n])
+def convertline(text, max_length=20):
+    text=text.split("\t") #split the conversation by tabs
+    inputs, targets=[],[] #create empty arrays for inputs and targets
+    for y in range(1,len(text)): #iterate through the split conversation
+        x=y-max_length if y-max_length >= 0 else 0 #get the starting value; if it's negative, use 0 instead
+        inputs.append("/b".join(text[x:y])) #append to the inputs the current window, joined by /b
+        targets.append(text[y]) #append the target
+    return [f"{inputs[i]}\t{targets[i]}" for i in range(len(inputs))] #zip them together in a dict of inputs and targets
 
-data=[]
-for file in os.listdir(args.dir):
-    for convo in tqdm(io.open(os.path.join(args.dir,file), mode="r", encoding="utf-8").read().strip().split("\n"), desc=f"Chunking {file}"):
-        dta=list(chunks(convo.split("\t"), args.chunks+1))
-        data.extend(dta)
-data=list(filter(None, data))
+def worker(filename, q):
+    with io.open(os.path.join(args.dir, filename), mode="r", encoding="utf-8") as f:
+        line = f.readline()
+        while line:
+            q.put(convertline(line.strip()))
+            line=f.readline()
+    return "DONE"
 
-with io.open(os.path.join(f"{args.out}-train.txt"), mode="w", encoding="utf-8") as t,  io.open(os.path.join(f"{args.out}-val.txt"), mode="w", encoding="utf-8") as v:
-    dist=np.random.choice(["t","v"], size=len(data), p=[args.split,1-args.split])
-    train=[e for i,e in enumerate(data) if dist[i]=="t"]
-    val=[e for i,e in enumerate(data) if dist[i]=="v"]
-    del data
-    del dist
-    if args.shuffle:
-        np.random.shuffle(train)
-        np.random.shuffle(val)
-    for line in tqdm(train, desc="Cleaning lines for train split"): 
-        bld=line.split("\t")[0]
-        for dta in line.split("\t")[1:]:
-            try:
-                if args.ascii: dta = dta.encode("ascii", "ignore").decode()
-                if dta.endswith("\\n"): dta[:-2]
-                if dta.split(": ")[1] != "" and dta.split(": ")[1] != " ":
-                    ln=bld+"\t"+dta.split(": ")[1]+"\n"
-                    if args.ascii: ln=ln.encode("ascii", "ignore").decode()
-                    t.write(ln.replace("\\n",args.newline_symbol))
-                    bld+=args.div_symbol+dta
-                    if len(bld.replace(args.div_symbol, " ")) >= int(args.max_len*1.5):break
-            except: pass
+def listener(q, split):
+    fst=True
+    with io.open(args.out+split+".txt", mode='w', encoding="utf-8") as f:
+        while 1:
+            m = q.get()
+            if m == 'kill':
+                f.write('killed')
+                break
+            for line in m:
+                if fst: f.write(line); fst=False
+                else: f.write("\n"+line)
+                f.flush()
 
-    for line in tqdm(val, desc="Cleaning lines for val split"): 
-        bld=line.split("\t")[0]
-        for dta in line.split("\t")[1:]:
-            try:
-                if args.ascii: dta = dta.encode("ascii", "ignore").decode()
-                if dta.endswith("\\n"): dta[:-2]
-                if dta.split(": ")[1] != "" and dta.split(": ")[1] != " ":
-                    ln=bld+"\t"+dta.split(": ")[1]+"\n"
-                    if args.ascii: ln=ln.encode("ascii", "ignore").decode()
-                    v.write(ln.replace("\\n",args.newline_symbol))
-                    bld+=args.div_symbol+dta
-                    if len(bld.replace(args.div_symbol, " ")) >= int(args.max_len*1.5):break
-            except: pass
+def main(files, split):
+    #must use Manager queue here, or will not work
+    manager = mp.Manager()
+    q = manager.Queue()    
+    pool = mp.Pool(mp.cpu_count() + 2)
+
+    #put listener to work first
+    watcher = pool.apply_async(listener, (q, split))
+
+    #fire off workers
+    jobs = []
+    for file in files:
+        job = pool.apply_async(worker, (file, q))
+        jobs.append(job)
+
+    # collect results from the workers through the pool result queue
+    for job in tqdm(jobs, desc=f"Processing {split}..."): 
+        job.get()
+
+    #now we are done, kill the listener
+    q.put('kill')
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+    files=os.path.join(args.dir).sort()
+    cut_off = int(len(files) * .05)
+    train_files, eval_files = files[:-cut_off], files[-cut_off:]
+    main(train_files, "train")
+    main(eval_files, "val")

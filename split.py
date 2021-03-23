d0bdd5412d8442c2e6cc2b25363dd5b3612334a7
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-23 16:29:14 -0700

    no locks YEET

--- split.py
+++ split.py
@@ -1,11 +1,10 @@
-#NOTE: This can be made more efficient by running it with multiprocessing, but I need to solve the issue with writing to one file
-
 import os
 import io
 import gzip
 import random
 import argparse
 from tqdm import tqdm
+import concurrent.futures
 
 parser = argparse.ArgumentParser(description='Split dataset in multiple files into train and validation sets')
 parser.add_argument('-dir', nargs="+", default=["data"],
@@ -16,27 +15,27 @@ parser.add_argument('-max_length', type=int, default=10,
                     help='maximum number of turns that the inputs amy have')
 parser.add_argument('-compression_level', type=int, default=9, choices=list(range(0,10)),
                     help='how compressed the file should be')
+parser.add_argument('-workers', type=int, default=4,
+                    help='number of workers to use (reccomended to be core count *2)')
 args = parser.parse_args()
 
-global fst
+global fst, w
 
-def worker(files, split, w):
-    global fst
-    for filename in tqdm(files, desc=f"{split} split..."):
-        with io.open(filename, mode="r", encoding="utf-8") as f:
-            line = f.readline()
-            while line:
-                line=line.strip().replace("\\n", "/n").split("\t")
-                for y in range(1,len(line)):
-                    x=y-args.max_length if y-args.max_length >= 0 else 0
-                    if args.compression_level != 0:
-                        if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8")); fst=False
-                        else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8"))
-                    else:
-                        if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}"); fst=False
-                        else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}")
-                line=f.readline()
-    return "DONE"
+def worker(filename):
+    global fst, lock, w
+    with io.open(filename, mode="r", encoding="utf-8") as f:
+        line = f.readline()
+        while line:
+            line=line.strip().replace("\\n", "/n").split("\t")
+            for y in range(1,len(line)):
+                x=y-args.max_length if y-args.max_length >= 0 else 0
+                if args.compression_level != 0:
+                    if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8")); fst=False
+                    else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8"))
+                else:
+                    if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}"); fst=False
+                    else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}")
+            line=f.readline()
 
 if __name__ == '__main__':
     files=[]
@@ -53,14 +52,18 @@ if __name__ == '__main__':
     if args.compression_level != 0:
         with gzip.open(f"{args.out}-train.txt.gz", "wb", compresslevel=args.compression_level) as w:
             fst=True
-            worker(train_files, "train", w)
+            with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
+                ret=list(tqdm(executor.map(worker, train_files), total=len(train_files), desc="Writing train..."))
         with gzip.open(f"{args.out}-val.txt.gz", "wb", compresslevel=args.compression_level) as w:
             fst=True
-            worker(eval_files, "val",w)
+            with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
+                ret=list(tqdm(executor.map(worker, eval_files), total=len(eval_files), desc="Writing val..."))
     else:
         with open(f"{args.out}-train.txt", "w") as w:
             fst=True
-            worker(train_files, "train", w)
+            with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
+                ret=list(tqdm(executor.map(worker, train_files), total=len(train_files), desc="Writing train..."))
         with open(f"{args.out}-val.txt", "w") as w:
             fst=True
-            worker(eval_files, "val",w)
+            with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
+                ret=list(tqdm(executor.map(worker, eval_files), total=len(eval_files), desc="Writing val..."))

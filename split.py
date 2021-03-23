50608f91c20157f710807341f1acbc9cee39a230
Jess Edmund Fan <jessefan2002@gmail.com>
2021-03-23 16:08:51 -0700

    more more fixes

--- split.py
+++ split.py
@@ -29,8 +29,12 @@ def worker(files, split, w):
                 line=line.strip().replace("\\n", "/n").split("\t")
                 for y in range(1,len(line)):
                     x=y-args.max_length if y-args.max_length >= 0 else 0
-                    if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8")); fst=False
-                    else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8"))
+                    if args.compression_level != 0:
+                        if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8")); fst=False
+                        else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}".encode("utf-8"))
+                    else:
+                        if fst: w.write(f"{'/b'.join(line[x:y])}\t{line[y]}"); fst=False
+                        else: w.write(f"\n{'/b'.join(line[x:y])}\t{line[y]}")
                 line=f.readline()
     return "DONE"
 
@@ -38,16 +42,25 @@ if __name__ == '__main__':
     files=[]
     for dir in args.dir:
         ofiles=os.listdir(dir)
-        ofiles.remove('stats.json')
+        try:ofiles.remove('stats.json')
+        except:pass
         files.extend(os.path.join(dir, i) for i in ofiles)
     random.shuffle(files)
     cut_off = int(len(files) * .05)
     train_files, eval_files = files[:-cut_off], files[-cut_off:]
-    print(f"Train size: {len(train_files)} files\tVal size: {len(eval_files)} files")
+    print(f"Train size: {len(train_files)} files\tVal size: {len(eval_files)} files\nFiles will be {'compressed' if args.compression_level != 0 else 'uncompressed'}.")
     
-    with gzip.open(f"{args.out}-train.txt.gz", "wb", compresslevel=args.compression_level) as w:
-        fst=True
-        worker(train_files, "train", w)
-    with gzip.open(f"{args.out}-val.txt.gz", "wb", compresslevel=args.compression_level) as w:
-        fst=True
-        worker(eval_files, "val",w)
+    if args.compression_level != 0:
+        with gzip.open(f"{args.out}-train.txt.gz", "wb", compresslevel=args.compression_level) as w:
+            fst=True
+            worker(train_files, "train", w)
+        with gzip.open(f"{args.out}-val.txt.gz", "wb", compresslevel=args.compression_level) as w:
+            fst=True
+            worker(eval_files, "val",w)
+    else:
+        with open(f"{args.out}-train.txt", "w") as w:
+            fst=True
+            worker(train_files, "train", w)
+        with open(f"{args.out}-val.txt", "w") as w:
+            fst=True
+            worker(eval_files, "val",w)

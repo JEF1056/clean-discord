ffe3c0a94fe79589fef166caffcfe9c082625834
Jess Edmund Fan <jessefan2002@gmail.com>
2021-04-01 12:25:48 -0700

    update commonq

--- personality.py
+++ personality.py
@@ -8,9 +8,9 @@ examples_convos=io.open("src/commonconvos.txt", mode="r", encoding="utf-8").read
 names=io.open("src/names.txt", mode="r", encoding="utf-8").read().strip().split("\n")
 
 max_len=10
-compression_level=9
+compression_level=0
 output_file="personality"
-partitions= 2000
+partitions= 1000
 
 def gen_all(line):
     line=line.strip().replace("\\n", "/n").split("\t")
@@ -25,20 +25,18 @@ def gen_all(line):
 out=[]
 val=[]
 
-print(gen_all(examples_convos[10].replace("[USER]", "Jake").replace("[BOT]", "Jade")))
-
 for example in tqdm(examples_pairs, "Generating Pairs..."):
     random.shuffle(names)
-    for name in names[:5000]:
+    for name in names[:1000]:
         out.append(example.replace("[USER]", name).replace("[BOT]", "Jade"))
-    for name in names[:250]:
+    for name in names[:50]:
         val.append(example.replace("[USER]", name).replace("[BOT]", "Jade"))
 
 for example in tqdm(examples_convos, "Generating Convos..."):
     random.shuffle(names)
-    for name in names[:2000]:
+    for name in names[:250]:
         out.extend(gen_all(example.replace("[USER]", name).replace("[BOT]", "Jade")))
-    for name in names[:100]:
+    for name in names[:2]:
         val.extend(gen_all(example.replace("[USER]", name).replace("[BOT]", "Jade")))
     
 random.shuffle(out)

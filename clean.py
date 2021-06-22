a125e0e3b8df07024855a1b14ca9bd1abe238936
Jess Edmund Fan <jessefan2002@gmail.com>
2021-06-21 20:10:26 -0700

    keep ret

--- clean.py
+++ clean.py
@@ -40,19 +40,19 @@ def run_regex():
     if args.overwrite: tasks=os.listdir(args.dir); print(f"Overwriting data in {args.out}")
     else: tasks=[m for m in os.listdir(args.dir) if re.search(r"\[\d{18}\](?:\s\[part \d{1,3}\])*", m).group(0)+".txt" not in os.listdir(args.out)]; print(f"Found {len(os.listdir(args.dir))-len(tasks)} files in {args.out}, skipping." if len(os.listdir(args.dir))-len(tasks) != 0 else f"Writing data to {args.out}")
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
-        list(tqdm(executor.map(worker_regex, tasks, repeat(args.dir), repeat(args.out), repeat(args.adv_prog)), total=len(tasks), desc="Cleaning..."))
+        ret=list(tqdm(executor.map(worker_regex, tasks, repeat(args.dir), repeat(args.out), repeat(args.adv_prog)), total=len(tasks), desc="Cleaning..."))
         
 def run_detox(to_clean):
     #precompute tasks and create required dir
     tasks=[f for f in os.listdir(to_clean) if f.endswith(".json")]
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
-        list(tqdm(executor.map(worker_detox, tasks, repeat(to_clean)), total=len(tasks), desc="Detoxifying..."))
+        ret=list(tqdm(executor.map(worker_detox, tasks, repeat(to_clean)), total=len(tasks), desc="Detoxifying..."))
     
 def run_antispam(to_clean):
     #precompute tasks and create required dir
     tasks=[f for f in os.listdir(to_clean) if f.endswith(".json")]
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
-        list(tqdm(executor.map(worker_antispam, tasks, repeat(to_clean)), total=len(tasks), desc="Antispam..."))
+        ret=list(tqdm(executor.map(worker_antispam, tasks, repeat(to_clean)), total=len(tasks), desc="Antispam..."))
         
 if __name__ == '__main__':
     if not args.skip_validation: check_files(args.dir)

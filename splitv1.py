22ac6c743796cccf9aeaeeccf64e903566987f6f
Jess Edmund Fan <jessefan2002@gmail.com>
2021-06-24 21:28:51 -0700

    ok

--- splitv1.py
+++ splitv1.py
@@ -4,7 +4,6 @@ import json
 import gzip
 import random
 import argparse
-import multiprocessing
 from tqdm import tqdm
 import concurrent.futures
 from itertools import repeat
@@ -27,35 +26,30 @@ args = parser.parse_args()
 
 try:os.mkdir("temp")
 except: pass
-lck=multiprocessing.Lock()
-fst= {"train": True, "val": True}
-if args.personality: 
-    personalities=json.load(io.open(args.personality, "r", encoding="utf-8"))
-    fst.update({"personality-train": True, "personality-val":True})
+if args.personality: personalities=json.load(io.open(args.personality, "r", encoding="utf-8"))
 
-def writefile(data, split):
-    with gzip.open(f"{args.out}-{split}.txt.gz", "ab+", compresslevel=args.compression_level) as f:
+def writefile(data, split, num):
+    fst=False
+    with gzip.open(f"{args.out}-{split}-{num}.txt.gz", "w", compresslevel=args.compression_level) as f:
         for line in data:
-            if fst[split]: 
-                lck.acquire()
+            if fst[split]:
                 f.write(f"{line}\n".encode("utf-8"))
                 fst[split]=False
-                lck.release()
             else: f.write(f"\n{line}".encode("utf-8"))
 
-def worker(filename, split, debug=False):
+def worker(filename, split, num, debug=False):
     if debug: profiler = Profiler(); profiler.start()
     temp, data, spl=[], json.load(io.open(filename, "r", encoding="utf-8")), split
     for conversation in data["conversations"]:
         spl=split
         per=not set([pair[1] for pair in conversation]).isdisjoint(list(personalities))
-        if per: spl="personality-"+spl
+        if per: spl="ps-"+spl
         if len(conversation) >= 2:
             for y in range(1,len(conversation)):
                 x=y-args.max_length if y-args.max_length >= 0 else 0
                 out=(f"persona: {(random.choice(personalities[str(conversation[y][1])]) if str(conversation[y][1]) in personalities else 'None').replace('	',' ')} context: {('/b'.join([msg[0] for msg in conversation[x:y]])).replace('	',' ')}\t{(': '.join(conversation[y][0].split(': ')[1:])).replace('	',' ')}").strip().replace("\\n", "/n").replace("\n","")
                 temp.append(out)
-    writefile(temp, spl)
+    writefile(temp, spl, num)
     if debug: profiler.stop(); print(profiler.output_text(unicode=True, color=True))
     return 0
     
@@ -70,7 +64,7 @@ if __name__ == '__main__':
     print(f"Train size: {len(train_files)} files\tVal size: {len(eval_files)} files")
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
-        ret=list(tqdm(executor.map(worker, train_files, repeat("train")), total=len(train_files), desc="Writing train..."))
+        ret=list(tqdm(executor.map(worker, train_files, repeat("train"), range(len(train_files))), total=len(train_files), desc="Writing train..."))
     #with gzip.open(f"{args.out}-val.txt.gz", "wb", compresslevel=args.compression_level) as w:
     with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
-        ret=list(tqdm(executor.map(worker, eval_files, repeat("val")), total=len(eval_files), desc="Writing val..."))
+        ret=list(tqdm(executor.map(worker, eval_files, repeat("val"), range(len(train_files))), total=len(eval_files), desc="Writing val..."))
